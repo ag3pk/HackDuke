1f651917d67f0fa3f50dc70ce257b4c036873379
Bethany Connor <bac5rc@virginia.edu>
2013-11-17 10:20:37 -0500

    Third time is a charm....

--- Compliments/src/hack/duke/compliments/MainActivity.java
+++ Compliments/src/hack/duke/compliments/MainActivity.java
@@ -17,6 +17,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.TextView;
 
+
 public class MainActivity extends Activity implements OnClickListener {
 
 	@Override
