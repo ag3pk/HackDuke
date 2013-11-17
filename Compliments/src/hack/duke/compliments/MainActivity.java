c2d3bad79a9b16de2bc98b1433065bc45f6a73bb
Bethany Connor <bac5rc@virginia.edu>
2013-11-17 04:41:27 -0500

    This should work once I can actually authenticate the database....
    I hope this commit doesn't break anything....

--- Compliments/src/hack/duke/compliments/MainActivity.java
+++ Compliments/src/hack/duke/compliments/MainActivity.java
@@ -1,5 +1,11 @@
 package hack.duke.compliments;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -18,12 +24,42 @@ public class MainActivity extends Activity implements OnClickListener {
 		TextView compliment1 = (TextView) findViewById(R.id.compliment1);
 		TextView compliment2 = (TextView) findViewById(R.id.compliment2);
 		TextView compliment3 = (TextView) findViewById(R.id.compliment3);
-		compliment1.setText("Compliment One.", TextView.BufferType.NORMAL);
-		compliment2.setText("Compliment Two.", TextView.BufferType.NORMAL);
-		compliment3.setText("Compliment Three.", TextView.BufferType.NORMAL);
-		compliment1.setOnClickListener(this);
-		compliment2.setOnClickListener(this);
-		compliment3.setOnClickListener(this);
+		try {
+			String[] complimentChoices = new String[3];
+			String json = "";
+
+			URL url = new URL("http://140db729.ngrok.com/Compliments/sqlPhp.php");
+			URLConnection conn = url.openConnection();
+
+			BufferedReader response = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			String line;
+			ArrayList<String> compliments = new ArrayList<String>();
+			while ((line = response.readLine()) != null) {
+				compliments.add(line);
+			}
+			ArrayList<Integer> used = new ArrayList<Integer>();
+			ArrayList<String> possibleCompliments = new ArrayList<String>();
+			for (int i =0; i < 3; i++) {
+				int rand = (int)(Math.random()*compliments.size());
+				if (!used.contains(rand)) {
+					complimentChoices[i] = compliments.get(rand);
+					used.add(rand);
+				}
+				else {
+					i--;
+				}
+			}
+
+			compliment1.setText(complimentChoices[0], TextView.BufferType.NORMAL);
+			compliment2.setText(complimentChoices[1], TextView.BufferType.NORMAL);
+			compliment3.setText(complimentChoices[2], TextView.BufferType.NORMAL);
+			compliment1.setOnClickListener(this);
+			compliment2.setOnClickListener(this);
+			compliment3.setOnClickListener(this);
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
 
 	}
 
@@ -37,7 +73,7 @@ public class MainActivity extends Activity implements OnClickListener {
 	public void onClick(View v) {
 		Intent nextScreen = new Intent(getApplicationContext(),
 				PhoneActivity.class);
-		
+
 		TextView compliment = (TextView) v;
 		// Sending data to another Activity
 		// nextScreen.putExtra("name", inputName.getText().toString());
